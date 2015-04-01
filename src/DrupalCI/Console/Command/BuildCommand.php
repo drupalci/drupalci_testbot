35f9b3bbfb8b2fbc5962d269d04630ae2a8bcd42
Karoly Negyesi <chx1975@gmail.com>
2015-04-01 14:32:09 -0700

    sync output

--- src/DrupalCI/Console/Command/BuildCommand.php
+++ src/DrupalCI/Console/Command/BuildCommand.php
@@ -66,12 +66,12 @@ class BuildCommand extends DrupalCICommandBase {
     $docker = $this->getDocker();
     $context = new Context($container_path);
     Output::writeln("-------------------- Start build script --------------------");
-    $response = $docker->build($context, $name, function ($output) use (&$content) {
+    $response = $docker->build($context, $name, function ($output) {
       if (isset($output['stream'])) {
-        $content .= $output['stream'];
+        Output::writeLn('<info>' . $output['stream'] . '</info>');
       }
     });
-    Output::writeLn("<info>$content</info>");
+
 
     //$response = $docker->build($context, $name, function ($result) use (&$content) {
     //  if (isset($result['stream'])) {
