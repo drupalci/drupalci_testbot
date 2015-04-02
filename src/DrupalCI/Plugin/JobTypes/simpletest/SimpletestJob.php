070090daef9f7c19d70f72a1cc62db222791cc62
Jeremy Thorson <jthorson@sasktel.net>
2015-04-02 10:38:13 -0600

    by jthorson: Temporarily disabling Simpletest job validation to facilitate testing

--- src/DrupalCI/Plugin/JobTypes/simpletest/SimpletestJob.php
+++ src/DrupalCI/Plugin/JobTypes/simpletest/SimpletestJob.php
@@ -44,12 +44,14 @@ class SimpletestJob extends JobBase {
     // yml job definition file, this information would be stored in the value
     // at array('environment' => array('db' => VALUE)); thus the traversal path
     // value is the array keys 'environment:db'
+    /* Temporarily hiding to prevent validation fails while testing during development
     'DCI_DBType' => 'environment:db',
     'DCI_DBVersion' => 'environment:db',
     'DCI_PHPVersion' => 'environment:web',
     'DCI_DrupalBranch' => 'variables:DCI_DrupalBranch',
     'DCI_DBUser' => 'variables:DCI_DBUser',
     'DCI_DBPassword' => 'variables:DCI_DBPassword',
+    */
   );
 
   /**
