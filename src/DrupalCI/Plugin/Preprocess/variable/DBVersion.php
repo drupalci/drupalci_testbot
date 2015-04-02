3b78bd6b696592a1b471c4b53c771d5e945e0448
Karoly Negyesi <chx1975@gmail.com>
2015-04-02 15:40:32 -0700

    completing dburl

--- src/DrupalCI/Plugin/Preprocess/variable/DBVersion.php
+++ src/DrupalCI/Plugin/Preprocess/variable/DBVersion.php
@@ -13,7 +13,11 @@ namespace DrupalCI\Plugin\Preprocess\variable;
 class DBVersion extends DBUrlBase {
 
   public function process($dci_variable, $value) {
-    return $this->buildUrl($dci_variable, 'scheme', explode('-', $value, 2)[0]);
+    $dbtype = explode('-', $value, 2)[0];
+    $host = 'drupaltestbot-db-' . str_replace('.', '-', $value);
+    $dci_variable = $this->buildUrl($dci_variable, 'scheme', $dbtype);
+    $dci_variable = $this->buildUrl($dci_variable, 'host', $host);
+    return $dci_variable;
   }
 
 }
