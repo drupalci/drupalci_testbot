fded7c9872872bfd9218388355657f33fda0d275
Karoly Negyesi <chx1975@gmail.com>
2015-04-01 21:22:57 -0700

    simplified

--- tests/DrupalCI/Tests/Plugin/BuildSteps/generic/ContainerCommandTest.php
+++ tests/DrupalCI/Tests/Plugin/BuildSteps/generic/ContainerCommandTest.php
@@ -18,7 +18,7 @@ use DrupalCI\Tests\DrupalCITestCase;
 class ContainerCommandTest extends DrupalCITestCase {
 
   function testRun() {
-    $cmd = ['test_command', 'test_argument'];
+    $cmd = 'test_command test_argument';
     $instance = new Container([]);
 
     $body = $this->getMock('GuzzleHttp\Stream\StreamInterface');
@@ -36,7 +36,7 @@ class ContainerCommandTest extends DrupalCITestCase {
       ->will($this->returnValue($instance));
     $container_manager->expects($this->once())
       ->method('exec')
-      ->with($instance, ['/bin/bash', '-c', implode(' ', $cmd)], TRUE, TRUE, TRUE, TRUE)
+      ->with($instance, ['/bin/bash', '-c', $cmd], TRUE, TRUE, TRUE, TRUE)
       ->will($this->returnValue(1));
     $container_manager->expects($this->once())
       ->method('execstart')
@@ -57,7 +57,7 @@ class ContainerCommandTest extends DrupalCITestCase {
       ->will($this->returnValue(['php' => [['id' => 'dockerci/php-5.4']]]));
 
     $command = new ContainerCommand([], 'command', []);
-    $command->run($this->job, [implode(' ', $cmd)]);
+    $command->run($this->job, $cmd);
   }
 
 }
