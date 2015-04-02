35ad7c0bf211e0c55646b1ea2bc30917f9c4fa2a
Karoly Negyesi <chx1975@gmail.com>
2015-04-01 18:54:49 -0700

    i really hate psr0

---
+++ tests/DrupalCI/Tests/Plugin/BuildSteps/generic/PatchTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * Contains \DrupalCI\Tests\Plugin\BuildSteps\generic\PatchTest.
+ */
+
+
+namespace DrupalCI\Tests\Plugin\BuildSteps\generic;
+
+
+use DrupalCI\Plugin\BuildSteps\setup\Patch;
+use DrupalCI\Tests\DrupalCITestCase;
+
+class PatchTest extends DrupalCITestCase {
+
+  function testRunwithFailingValidation() {
+    $patch = new TestPatch([], 'patch', []);
+    $patch->setValidate(FALSE);
+    $data = [
+      ['patch_file' => 'test.file', 'patch_dir' => 'patch/dir'],
+    ];
+    $patch->run($this->job, $data);
+    $this->assertEmpty($patch->getCommands());
+  }
+
+
+
+}
+
+class TestPatch extends Patch {
+
+  protected $commands = [];
+
+  protected $validate = TRUE;
+
+  protected $execResult;
+
+  function exec($command, &$output, &$result) {
+    $this->commands[] = $command;
+    return $this->execResult;
+  }
+
+  function getCommands() {
+    return $this->commands;
+  }
+
+  function validateDirectory() {
+    return $this->validate;
+  }
+
+  function setValidate($validate) {
+    $this->validate = $validate;
+  }
+
+  function setExecResult($exec_result) {
+    $this->execResult = $exec_result;
+  }
+}
