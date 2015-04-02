74ee4d57fcbc17de4b61a0557e62707491be33f8
Karoly Negyesi <chx1975@gmail.com>
2015-04-01 19:29:51 -0700

    full patchtest

--- tests/DrupalCI/Tests/Plugin/BuildSteps/generic/PatchTest.php
+++ tests/DrupalCI/Tests/Plugin/BuildSteps/generic/PatchTest.php
@@ -15,18 +15,38 @@ use DrupalCI\Tests\DrupalCITestCase;
 
 class PatchTest extends DrupalCITestCase {
 
-  function testRunwithFailingValidation() {
+  /**
+   * @dataProvider runProvider
+   */
+  public function testRun($error_count, $validate, $exec_result, $commands) {
+    $this->job->expects($this->exactly($error_count))
+      ->method('errorOutput');
     $patch = new TestPatch([], 'patch', []);
-    $patch->setValidate(FALSE);
+    $args = [
+      '%dir' => 'patch/dir',
+      '%file' => 'test.file',
+    ];
+    $patch->setValidate(strtr($validate, $args));
+    $patch->setExecResult($exec_result);
     $data = [
-      ['patch_file' => 'test.file', 'patch_dir' => 'patch/dir'],
+      ['patch_file' => $args['%file'], 'patch_dir' => $args['%dir']],
     ];
     $patch->run($this->job, $data);
-    $this->assertEmpty($patch->getCommands());
+    if ($commands) {
+      $commands = array_map(function ($command) use ($args) {
+        return strtr($command, $args);
+      }, $commands);
+      $this->assertSame($commands, $patch->getCommands());
+    }
   }
 
-
-
+  public function runProvider() {
+    return [
+      [1, FALSE, 1, []],
+      [1, '%dir', 1, ["patch -p1 -i %file -d %dir"]],
+      [0, '%dir', 0, ["patch -p1 -i %file -d %dir"]],
+    ];
+  }
 }
 
 class TestPatch extends Patch {
@@ -39,7 +59,7 @@ class TestPatch extends Patch {
 
   function exec($command, &$output, &$result) {
     $this->commands[] = $command;
-    return $this->execResult;
+    $result = $this->execResult;
   }
 
   function getCommands() {
