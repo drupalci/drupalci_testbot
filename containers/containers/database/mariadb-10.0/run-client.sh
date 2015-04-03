7d97ba628ea555d6d8e1c760bee041d1cf92b8d6
Jeremy Thorson <jthorson@sasktel.net>
2015-04-03 14:41:36 -0600

    by jthorson: Container sync cleanup

--- containers/containers/database/mariadb-10.0/run-client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TAG="drupalci/mariadb-10.0"
-CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
-IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
-
-echo $IP
-mysql -u drupaltestbot -pdrupaltestbotpw -h $IP
--- containers/containers/database/mysql-5.5/run-client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TAG="drupalci/mysql-5.5"
-CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
-IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
-
-echo $IP
-mysql -u drupaltestbot -pdrupaltestbotpw -h $IP
-
