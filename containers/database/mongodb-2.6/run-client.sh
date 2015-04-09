bf900448db18f947f5b727b9157403db68831333
Rudy Grigar <basic@drupal.org>
2015-04-08 18:13:06 -0700

    Move all data to Drupal.org - https://www.drupal.org/project/drupalci_testbot

--- containers/database/mongodb-2.6/run-client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TAG="drupalci/mongodb-2.6"
-CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
-IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
-
-echo $IP
-mongo $IP/drupal
